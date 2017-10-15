518600787d1f7bad1891635c883fac4650fa173f
Giovani Camara <Gcamara14@gmail.com>
2017-10-15 01:17:48 -0700

    Adds UI to the signup form and debuggs signup error

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   get 'welcome/contact'
 
 
-  root 'welcome#index'
+  root to: 'welcome#index'
+
 
   resources :businesses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
