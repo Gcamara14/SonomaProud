a5f62704d4393bf6882aa64ca49514bffff7c14a
Giovani Camara <Gcamara14@gmail.com>
2017-10-15 03:21:08 -0700

    adds final show page and accessibility edits

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,5 @@
 class Business < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://s3-us-west-1.amazonaws.com/recipe-app-gio/recipes/images/000/000/008/medium/sonomaProud.png"
+  has_attached_file :image, styles: {  large: "750x750" ,medium: "300x300>", thumb: "100x100>" }, default_url: "https://s3-us-west-1.amazonaws.com/recipe-app-gio/recipes/images/000/000/008/medium/sonomaProud.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   extend FriendlyId
